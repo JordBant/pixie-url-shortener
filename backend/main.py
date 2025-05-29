87f48855c7b12ef08cd5c28d07010411bee80287
JordBant <jaybdev25@gmail.com>
2025-05-29 16:44:07 -0400

    Added items to gitignore

--- backend/main.py
+++ backend/main.py
@@ -5,3 +5,12 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "It works!"}
+
+'''
+
+1. Make hashing funcition return a hashed string
+2. Store in an object
+3. Make User class
+    
+
+'''
