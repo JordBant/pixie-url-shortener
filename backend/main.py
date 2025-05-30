5e1635dfb488b2e476b94e77f16344428ec18331
JordBant <jaybdev25@gmail.com>
2025-05-29 21:16:22 -0400

    Writing Hashing logic

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,39 @@
 from fastapi import FastAPI
+from hashlib import sha256
+
+# from models.helpers import hash_and_encode
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "It works!"}
 
-'''
+# @app.get("/")
+# def read_root():
+#     # hash_and_encode("Testing out this string")
+#     return hash_and_encode("Testing out this string")
 
-1. Make hashing funcition return a hashed string
-2. Store in an object
-3. Make User class
+def hash_and_encode(input: str, isCustom = False):
+    hashed = sha256(input.encode('utf-8')).hexdigest()
+    encoded = int(hashed, 16)
+
+    if 
+
+# class User
+
+def main():
+    hexed = hash_and_encode("hi im paul")
+    print(hexed)
+    print(int(hexed, 16))
     
 
-'''
+if __name__ == "__main__":
+    main()
+
+"""
+
+1. Make hashing function return a hashed string MD5 or SHA-256
+    then make it encoded base =62
+2. Store in an object
+3. Make User class
+
+
+"""
